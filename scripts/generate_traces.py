731c40f3b8627f2bca7fe15b5a366cf2a5e8074a
Sergiolopezflores <138206863+Sergiolopezflores@users.noreply.github.com>
2025-03-05 10:27:26 +0100

    Add files via upload

---
+++ scripts/generate_traces.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import paramiko
+import os
+import random
+import argparse
+from threading import Thread
+from tqdm import trange
+import time
+
+def randomString(stringLength=16):
+    """Generate a random hex-string of fixed length """
+    return ''.join(chr(random.randrange(256)).encode("ISO-8859-1").hex() for _ in range(stringLength))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cmdFile', type=str, help="Path to a file containing the commands to launch.")
+    parser.add_argument('-c', type=int, default=100, dest='count', help='Number of traces')
+    parser.add_argument('-w', type=str, default="/home/pi/wrapper", dest='wrapperPath', help='Path of the wrapper program')
+    args = parser.parse_args()
+
+    # Parse command file
+    cmdLines = []
+    with open(args.cmdFile) as f:
+        for line in f:
+            if len(line) > 1:
+                cmdLines.append(line.strip("\n").split(","))
+
+    # Initialize SSH connection
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect('192.168.128.37', username='ocarlon', password='TFG34')
+    ssh_transp = ssh.get_transport()
+
+    for _ in trange(args.count):
+        for pre, cmd, tag in cmdLines:
+            dataFile = "trace-{}.dat".format(randomString(16))
+            print('{} "{}"'.format(args.wrapperPath, cmd))
+            if pre:
+                chan = ssh_transp.open_session()
+                chan.exec_command(pre)
+                while not chan.exit_status_ready():
+                    time.sleep(0.01)
+                chan.close()
+            # Launch command
+            chan = ssh_transp.open_session()
+            chan.exec_command('{} "{}"'.format(args.wrapperPath, cmd))
+            chan.close()
+    
+    ssh.close()
